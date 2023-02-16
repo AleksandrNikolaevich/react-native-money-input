3ac536c1159da1d77c6a766d84d1177423c46590
Александр Чернов <aleksandr@MBP-Aleksandr-2.login.stonet.cc>
2023-02-16 10:21:34 +0100

    test(jest): fixed tests

--- jest/setup.js
+++ jest/setup.js
@@ -1,16 +1,21 @@
 const mockComponent = jest.requireActual(
-  '../node_modules/react-native/jest/mockComponent.js'
+  '../../../react-native/jest/mockComponent.js'
 );
 
 const MockNativeMethods = jest.requireActual(
-  '../node_modules/react-native/jest/MockNativeMethods'
+  '../../../react-native/jest/MockNativeMethods'
 );
 
 jest.mock('@alexzunik/react-native-money-input', () => {
-  return mockComponent('../Libraries/Components/TextInput/TextInput', {
-    ...MockNativeMethods,
-    isFocused: jest.fn(),
-    clear: jest.fn(),
-    getNativeRef: jest.fn(),
-  });
+  return {
+    MoneyTextInput: mockComponent(
+      '../Libraries/Components/TextInput/TextInput',
+      {
+        ...MockNativeMethods,
+        isFocused: jest.fn(),
+        clear: jest.fn(),
+        getNativeRef: jest.fn(),
+      }
+    ),
+  };
 });
